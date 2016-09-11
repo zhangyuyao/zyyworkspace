d75f1fc66ebb9907603573d1d700da5e32bf6b2b
高研老师1 <gaoyanlaoshi1@qiunandeMacBook-Pro.local>
2016-09-11 22:12:26 +0800

    firstime

---
+++ FirstTime/FirstTimeTests/FirstTimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  FirstTimeTests.swift
+//  FirstTimeTests
+//
+//  Created by 高研老师1 on 16/9/11.
+//  Copyright © 2016年 高研老师1. All rights reserved.
+//
+
+import XCTest
+@testable import FirstTime
+
+class FirstTimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
